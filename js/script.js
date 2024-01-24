6bf71a589501b2c963a3abb2e96b88df6a03ad59
6hayoung <6yue@kakao.com>
2024-01-24 09:22:54 +0900

    'editCss'

--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
 $(function(){
-    console.log('스크립트 보기 >')
-    $('#left > ul > li').on('click',function(){
-        let leftIdxM = $(this).index();
-            $('#right > .slide-wrapper > div').eq(leftIdxM).fadeIn().siblings().fadeOut()
-    })          
 
+    //console.log('스크립트 보기 >')
 
-    //타이틀 변경
+    //GNB 클릭 이벤트
     $('#left > ul > li').on('click',function(){
         let leftIdx = $(this).index();
         const menuEng = ['HOME','PROFILE','ABILITY','PORTFOLIO','THANK YOU']
+        $('#right > .slide-wrapper > div').eq(leftIdx).fadeIn().siblings().fadeOut()
         $(this).addClass('view').siblings().removeClass('view')
         $('.header > .pagination').text(menuEng[leftIdx])
-    })
+    })          
 
-    $('.pf-cont-tit > p').on('mouseover',function(){
-        $(this).addClass('view')
-    })
-    $('.pf-cont-tit > p').on('mouseleave',function(){
-        $(this).removeClass('view')
-    })
 })
